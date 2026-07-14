c8b241ec72ee60bf9fbbaa6dfc555d1358412a7f
Aayush <aayushbastola772@gmail.com>
2026-07-14 20:16:57 +0545

    Custom text class

--- lib/common/custom_text.dart
+++ lib/common/custom_text.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart';
 class CustomText extends StatelessWidget {
   final String text;
   final double fontSize;
-  const CustomText({super.key, required this.text, this.fontSize = 30});
+  final FontWeight fontWeight;
+
+  const CustomText({
+    super.key,
+    required this.text,
+    this.fontSize = 18,
+    this.fontWeight = FontWeight.w400,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(fontSize: fontSize, color: Colors.blue),
+      style: TextStyle(
+        fontSize: fontSize,
+        color: Colors.black,
+        fontWeight: fontWeight,
+      ),
     );
   }
 }
