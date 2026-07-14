2ba4c76674b2cef337a06c72f5a63a2c404b4112
Aayush <aayushbastola772@gmail.com>
2026-07-14 20:17:43 +0545

    called acme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/profile_page.dart';
+import 'package:flutter_application_1/acme_home_page.dart';
+// import 'package:flutter_application_1/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Profile page', home: const ProfilePage());
+    // return MaterialApp(title: 'Profile page', home: const ProfilePage());
+    return MaterialApp(title: "Acme Home Page", home: const AcmeHomePage());
   }
 }
