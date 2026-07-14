aaf04555e4e4bd789520af07c8b7929f606ccdd8
Aayush <aayushbastola772@gmail.com>
2026-07-14 19:02:28 +0545

    Initial commit

---
+++ lib/common/custom_text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  const CustomText({super.key, required this.text, this.fontSize = 30});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: fontSize, color: Colors.blue),
+    );
+  }
+}
