b1d2577e7fb39ea79bc6fdda2d2f25260b73c511
Aayush <aayushbastola772@gmail.com>
2026-07-16 22:30:50 +0545

    Custom card for contact

---
+++ lib/common/common_contact_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/common/custom_text.dart';
+
+class CommonContactCard extends StatelessWidget {
+  final String name;
+  final String number;
+  final Color color;
+
+  const CommonContactCard({
+    super.key,
+    required this.name,
+    required this.number,
+    this.color = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+                color: color,
+              ),
+            ),
+            Positioned(
+              left: 2.5,
+              child: Icon(Icons.person_rounded, size: 55, color: Colors.white),
+            ),
+          ],
+        ),
+        SizedBox(width: 20),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomText(text: name, color: Colors.black),
+            CustomText(
+              text: number,
+              color: const Color.fromARGB(255, 93, 92, 92),
+            ),
+          ],
+        ),
+        Spacer(),
+        Icon(
+          Icons.keyboard_arrow_right,
+          size: 40,
+          color: const Color.fromARGB(255, 93, 92, 92),
+        ),
+      ],
+    );
+  }
+}
