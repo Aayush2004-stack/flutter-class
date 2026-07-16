3ba75a796f9f4ad576f44163881f53e15188995d
Aayush <aayushbastola772@gmail.com>
2026-07-16 22:31:01 +0545

    Contact list page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/contact_page.dart';
+import 'package:flutter_application_1/contact_list_page.dart';
+//import 'package:flutter_application_1/contact_page.dart';
 //import 'package:flutter_application_1/acme_home_page.dart';
-import 'package:flutter_application_1/profile_page.dart';
+//import 'package:flutter_application_1/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +14,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Profile page', home: const ContactPage());
-   
+    return MaterialApp(title: "Contact List", home: const ContactListPage());
+    //return MaterialApp(title: 'Profile page', home: const ContactPage());
+
     //return MaterialApp(title: "Acme Home Page", home: const AcmeHomePage());
   }
 }
