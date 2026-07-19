6d68d55c5e8370d54bbe9e826d37b4cb8b5b7506
Aayush <aayushbastola772@gmail.com>
2026-07-19 18:01:52 +0545

    Done in class

--- lib/common/custom_text.dart
+++ lib/common/custom_text.dart
@@ -11,7 +11,7 @@ class CustomText extends StatelessWidget {
     required this.text,
     this.fontSize = 18,
     this.fontWeight = FontWeight.w400,
-    this.color = Colors.green,
+    this.color = const Color.fromARGB(255, 0, 1, 0),
   });
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/contact_list_page.dart';
+import 'package:flutter_application_1/item_page.dart';
 //import 'package:flutter_application_1/contact_page.dart';
 //import 'package:flutter_application_1/acme_home_page.dart';
 //import 'package:flutter_application_1/profile_page.dart';
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Contact List", home: const ContactListPage());
+    return MaterialApp(title: "Items", home: ItemPage());
+    //return MaterialApp(title: "Contact List", home: const ContactListPage());
     //return MaterialApp(title: 'Profile page', home: const ContactPage());
 
     //return MaterialApp(title: "Acme Home Page", home: const AcmeHomePage());
